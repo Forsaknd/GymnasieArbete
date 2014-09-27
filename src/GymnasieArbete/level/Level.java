1d51b6b82fb49e37ea3b222d93582993d2ce94cf
Forsaknd <Forsaknkallstrom@gmail.com>
2014-09-27 13:01:58 +0200

    randomlevel finito

--- src/GymnasieArbete/level/Level.java
+++ src/GymnasieArbete/level/Level.java
@@ -34,9 +34,9 @@ public class Level {
 	public void render(int xScroll, int yScroll, Screen screen) {
 		screen.setOffset(xScroll, yScroll);
 		int x0 = xScroll >> 4;
-		int x1 = (xScroll + screen.width) >> 4;
+		int x1 = (xScroll + screen.width + 16) >> 4;
 		int y0 = yScroll >> 4;
-		int y1 = (yScroll + screen.height) >> 4;
+		int y1 = (yScroll + screen.height + 16) >> 4;
 		
 		for(int y = y0; y < y1; y++) {
 			for(int x = x0; x < x1; x++) {
@@ -46,8 +46,8 @@ public class Level {
 	}
 
 	public Tile getTile(int x, int y) {
-		if (tiles[x + y * width] == 0)
-			return Tile.grass;
+		if (x < 0 || y < 0 || x >= width || y >= height) return Tile.voidTile;
+		if (tiles[x + y * width] == 0) return Tile.grass;
 		return Tile.voidTile;
 	}
 
